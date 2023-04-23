8f14f2eee98eaf5aa8819ca23715f9d4c3df9f27
Simone <simon.muscas@gmail.com>
2023-04-23 23:05:35 +0200

    install nerdfont

--- install-apt.sh
+++ install-apt.sh
@@ -1,4 +1,5 @@
 sudo apt-get install cbonsai -y
+sudo apt-get install -y dbus-x11
 sudo apt-get install cmatrix -y
 sudo apt-get install taskwarrior -y
 sudo apt-get install inotify-tools -y
@@ -6,3 +7,6 @@ sudo apt-get install python3-pip -y
 pip3 install --user pynvim -y
 sudo apt-get install wmctrl -y
 sudo apt-get install tmuxifier -y
+
+sudo apt-get install xsel -y
+sudo apt-get install xclip -y
---
+++ install_font.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update package index
+sudo apt-get update
+
+# Install necessary packages
+sudo apt-get install -y wget unzip fontconfig dconf-cli
+
+# Create the fonts directory if it doesn't exist
+mkdir -p ~/.local/share/fonts
+
+# Download and extract FuraMono Nerd Font
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip -O /tmp/FiraMono.zip
+unzip /tmp/FiraMono.zip -d ~/.local/share/fonts/FiraMonoNerdFont
+
+# Update font cache
+fc-cache -fv
+
+# Set FuraMono Nerd Font Mono as the default terminal font
+PROFILE_ID=$(dconf list /org/gnome/terminal/legacy/profiles:/ | grep -E '^:[a-z0-9]+/' | head -n1)
+dconf write /org/gnome/terminal/legacy/profiles:/${PROFILE_ID}font "'FuraMono Nerd Font Mono 12'"
+
+# Clean up
+rm /tmp/FiraMono.zip
+
+echo "FuraMono Nerd Font Mono has been installed and set as the default terminal font."
